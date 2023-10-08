3eb80fa2be6b57700b08c51a4bc7d3db791dc00d
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-10-08 14:00:59 +0700

    update

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Typography } from "@mui/material";
+import { Typography } from "@mui/material";
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
