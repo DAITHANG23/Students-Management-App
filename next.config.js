d1907a20d5f4da161a4c438ee853109bb092a4cc
DAITHANG23 <nguyendaithang23061997@gmail.com>
2023-10-08 16:26:18 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,21 +1,25 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   experimental: {
-//     appDir: false,
-//   },
-// };
-
-// module.exports = nextConfig;
-
-module.exports = (phase, { defaultConfig }) => {
-  /**
-   * @type {import('next').NextConfig}
-   */
-  const nextConfig = {
-    /* config options here */
-    experimental: {
-      appDir: false,
-    },
-  };
-  return nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // experimental: {
+  //   appDir: false,
+  // },
 };
+
+module.exports = {
+  experimental: {
+    appDir: false,
+  },
+};
+
+// module.exports = (phase, { defaultConfig }) => {
+//   /**
+//    * @type {import('next').NextConfig}
+//    */
+//   const nextConfig = {
+//     /* config options here */
+//     experimental: {
+//       appDir: false,
+//     },
+//   };
+//   return nextConfig;
+// };
